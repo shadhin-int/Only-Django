b279154bd25e7d860f163fb9850e7087c4aa9acc
shadhin <shahinur.rahman@evaly.com.bd>
2021-10-05 03:09:57 +0600

    complete project setup and generate meme from api in view

--- MEME/app/views.py
+++ MEME/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+import requests
+
+
+def getdata(request):
+	response = request.get('https://meme-api.herokuapp.com/gimme').json()
+	context = {
+		'url': response['url']
+	}
+
+	return render(request, 'index.html', context)
