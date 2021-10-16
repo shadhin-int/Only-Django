4eea0cb628c42e8dd2ed9865fae3c9289ae6c5ee
shadhin <shadhin.int@gmail.com>
2021-10-17 03:49:15 +0600

    added task details view

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 
-def taskList(request):
-	return HttpResponse('To Do List')
+class TaskList(ListView):
+	model = Task
+	context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+	model = Task
+	context_object_name = 'task'
+	template_name = 'base/task.html'
