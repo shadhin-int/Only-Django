a1e7f0a1a72fb95da0352c964443730ce5e05be9
shadhin <shadhin.int@gmail.com>
2021-10-19 04:10:03 +0600

    redirect login page from registration

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -26,6 +26,12 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
