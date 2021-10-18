c60f6bccdf64753e9b58178ace35cc5bec80f745
shadhin <shadhin.int@gmail.com>
2021-10-19 02:40:39 +0600

    user specific data

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -22,6 +22,12 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
