37fc9d18c2a27057cc620681eff0c1ba7a406afa
shadhin <shadhin.int@gmail.com>
2021-10-19 03:01:45 +0600

    refactor for select self user to create task

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -37,13 +37,17 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
